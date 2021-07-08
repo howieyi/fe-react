066f3e28a260fc7ebcbb511b02213163214658ba
水逆 <huyi247767221@163.com>
2021-07-08 17:00:56 +0800

    lerna publish

--- packages/wechat/dist/cjs/login/login.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 微信网页授权登录
- *
- * @param appId
- * @returns
- */
-export declare const loginWeChat: (appId: string) => string;
--- packages/wechat/dist/esm/login.js
+++
@@ -1,9 +0,0 @@
-/**
- * 微信网页授权登录
- *
- * @param appId
- * @returns
- */
-export const loginWeChat = (appId) => {
-    return appId;
-};
