0b96cc52776932ba77d56016bd5abcc3e8bec2d7
水逆 <huyi247767221@163.com>
2021-07-18 03:56:49 +0800

    feat: add eslint and commitlint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'airbnb-typescript', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    project: './tsconfig.eslint.json',
+  },
+  rules: {},
+};
