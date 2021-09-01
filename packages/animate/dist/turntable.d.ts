2a903fdcbee49b94ba2e8a346d7dae62046707df
水逆 <>
2021-09-01 18:41:05 +0800

    feat: 新增常规动画处理

---
+++ packages/animate/dist/turntable.d.ts
@@ -0,0 +1,15 @@
+/**
+ * 缓动算法
+ *
+ * @param t 当前时间
+ * @param b 初始值
+ * @param c 变化量
+ * @param d 持续时间
+ * @returns
+ */
+export declare const easeInOut: (
+  t: number,
+  b: number,
+  c: number,
+  d: number,
+) => number;
