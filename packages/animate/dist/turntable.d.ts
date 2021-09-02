c28a7b0554638277529c94bcf3cd25c582418800
水逆 <>
2021-09-02 17:50:44 +0800

    feat: animate build

--- packages/animate/dist/turntable.d.ts
+++ packages/animate/dist/turntable.d.ts
@@ -13,3 +13,36 @@ export declare const easeInOut: (
   c: number,
   d: number,
 ) => number;
+/**
+ * 转盘游戏
+ */
+export declare class Turntable {
+  private result;
+  private usedTime;
+  private duration;
+  private step;
+  constructor(step: number, duration: number);
+  /**
+   * 设置初始参数
+   *
+   * @param usedTime
+   * @param step
+   * @param duration
+   */
+  setOptions: (step?: number, duration?: number) => void;
+  /**
+   * 旋转
+   *
+   * @param isContinue
+   * @returns
+   */
+  rotate: (isContinue: (angel: number, usedTime: number) => boolean) => void;
+  /**
+   * 获取旋转到第几块
+   *
+   * @param angel
+   * @param split
+   * @returns
+   */
+  getRotateResult: (angel: number, split?: number) => number;
+}
--- packages/animate/dist/turntable.js
+++ packages/animate/dist/turntable.js
@@ -1,6 +1,6 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
-exports.easeInOut = void 0;
+exports.Turntable = exports.easeInOut = void 0;
 /**
  * 缓动算法
  *
@@ -16,3 +16,59 @@ const easeInOut = (t, b, c, d) => {
   return (-c / 2) * (--t * (t - 2) - 1) + b;
 };
 exports.easeInOut = easeInOut;
+/**
+ * 转盘游戏
+ */
+class Turntable {
+  constructor(step, duration) {
+    this.result = 0;
+    this.usedTime = 0; // 已经旋转的时间
+    /**
+     * 设置初始参数
+     *
+     * @param usedTime
+     * @param step
+     * @param duration
+     */
+    this.setOptions = (step = 0, duration = 0) => {
+      this.duration = duration || Math.random() * 3 * 4000;
+      this.step = step || Math.random() * 10 + 10;
+    };
+    /**
+     * 旋转
+     *
+     * @param isContinue
+     * @returns
+     */
+    this.rotate = isContinue => {
+      this.usedTime += 20;
+      const { step, usedTime, duration } = this;
+      if (usedTime >= duration) {
+        isContinue && isContinue(this.result, this.usedTime);
+        this.usedTime = 0;
+        this.result = 0;
+        return;
+      }
+      const change =
+        (step - (0, exports.easeInOut)(usedTime, 0, step, duration)) *
+        (Math.PI / 180);
+      this.result += (change * 180) / Math.PI;
+      // 继续条件
+      (!isContinue || (isContinue && isContinue(this.result, this.usedTime))) &&
+        window.requestAnimationFrame(() => this.rotate(isContinue));
+    };
+    /**
+     * 获取旋转到第几块
+     *
+     * @param angel
+     * @param split
+     * @returns
+     */
+    this.getRotateResult = (angel, split = 9) => {
+      const idx = (angel % 360) / (360 / split);
+      return idx;
+    };
+    this.setOptions(step, duration);
+  }
+}
+exports.Turntable = Turntable;
